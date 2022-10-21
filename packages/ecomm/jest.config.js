f36ad147547aee9568e70bf2c90d03328dc842bd
kishormaharjan <kishor@freely.me>
2022-10-21 20:11:40 +1100

    jest config

--- packages/ecomm/jest.config.js
+++ packages/ecomm/jest.config.js
@@ -15,6 +15,21 @@ const customJestConfig = {
         '^@/pages/(.*)$': '<rootDir>/pages/$1',
     },
     testEnvironment: 'jest-environment-jsdom',
+    collectCoverageFrom: [
+        './pages/**/*.{ts,tsx}',
+        '!**/node_modules/**',
+        '!**/vendor/**',
+        '!**/*.{types, stories, test, spec, constants}.{ts, tsx}',
+    ],
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
