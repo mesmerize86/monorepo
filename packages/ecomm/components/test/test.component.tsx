b20d4003fef403bc9a19bbe96ada923ceeaab8bc
kishormaharjan <kishor@freely.me>
2022-10-21 15:01:43 +1100

    fix sonar issue

--- packages/ecomm/components/test/test.component.tsx
+++ packages/ecomm/components/test/test.component.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 export const Test = () => {
-    const trial = 'abc';
-    const datas = [{a: 'b', b: 'c'}]
     return (
         <div>
             <h1>Hello Test</h1>
