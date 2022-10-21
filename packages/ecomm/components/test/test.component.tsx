587c822fef0d27b291f8e08cd9f7e20f244d3c44
kishormaharjan <kishor@freely.me>
2022-10-21 14:51:42 +1100

    testing sonar error

--- packages/ecomm/components/test/test.component.tsx
+++ packages/ecomm/components/test/test.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const Test = () => {
     const trial = 'abc';
+    const datas = [{a: 'b', b: 'c'}]
     return (
         <div>
             <h1>Hello Test</h1>
