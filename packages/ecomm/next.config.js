6292dd23c4784a44f89c4f7cbbd976ec9f411e2b
kishormaharjan <kishor@freely.me>
2022-10-28 14:29:38 +1100

    checks

--- packages/ecomm/next.config.js
+++ packages/ecomm/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    dirs: ['pages'], // Only run ESLint on the 'pages' during production builds (next build)
+  },
 }
 
 module.exports = nextConfig
