f2832ea4c080b450cda8a5927ebca52302fcb619
kishorfreely <kishor@freely.me>
2022-11-02 14:57:33 +1100

    demo test

--- packages/ecomm/pages/index.tsx
+++ packages/ecomm/pages/index.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+    const test = '';
+    const test = '';
   return (
     <div className={styles.container}>
       <Head>
