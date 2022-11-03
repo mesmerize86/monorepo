e039c557ef97ac6706dab02da49589b411cd922a
kishorfreely <kishor@freely.me>
2022-11-03 11:06:35 +1100

    jest - 9

--- packages/ecomm/pages/home/home.view.tsx
+++ packages/ecomm/pages/home/home.view.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export const Home = () => {
+    const arr = [4, 3, 2, 1];
+
+    for (let value of arr) {
+        console.log('value', value);
+    }
     return (
         <div>
             <h1>Welcome to next js</h1>
