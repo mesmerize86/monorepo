2ccdc12f4df1762e7e4c7c659737139415b68996
kishorfreely <kishor@freely.me>
2022-11-11 16:39:02 +1100

    unit test

--- packages/ecomm/pages/home/home.test.tsx
+++ packages/ecomm/pages/home/home.test.tsx
@@ -1,14 +1,20 @@
-import { render, screen } from '@testing-library/react'
-import {Home} from './index'
+import { render, screen } from "@testing-library/react";
+import { Home } from "./index";
+import { sum } from "./util";
 
-describe('Home', () => {
-    it('renders a heading', () => {
-        render(<Home />)
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
 
-        const heading = screen.getByRole('heading', {
-            name: 'Welcome to next js',
-        })
+    const heading = screen.getByRole("heading", {
+      name: "Welcome to next js",
+    });
 
-        expect(heading).toBeInTheDocument()
-    })
-})
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("add a sum", () => {
+    const result = sum(4, 5);
+    expect(result).toBe(9);
+  });
+});
